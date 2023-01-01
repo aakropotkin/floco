627876880a2ce35e60636b3766e6abeed8a628f2
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-01 15:08:38 -0600

    strip declaration path in module docs

--- doc/modules/make-options-doc.nix
+++ doc/modules/make-options-doc.nix
@@ -8,11 +8,16 @@
 , lib     ? nixpkgs.lib
 , system  ? builtins.currentSystem
 , pkgsFor ? nixpkgs.legacyPackages.${system}
+, basedir ? toString ../..
 , options
 }: import "${nixpkgs}/nixos/lib/make-options-doc" {
   inherit lib;
   pkgs    = pkgsFor;
   options = removeAttrs options ["_module"];
+  transformOptions = optDoc: optDoc // {
+    declarations = map ( builtins.replaceStrings [basedir] ["/floco"] )
+                       optDoc.declarations;
+  };
 }
 
 
