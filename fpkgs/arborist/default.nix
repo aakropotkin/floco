ab20e8f02b5261b3b4704b4c673af200908d05e0
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-05 20:31:51 -0600

    add arborist package

---
+++ fpkgs/arborist/default.nix
@@ -0,0 +1,30 @@
+# ============================================================================ #
+#
+# Package shim exposing installable targets from `floco` modules.
+#
+# ---------------------------------------------------------------------------- #
+
+{ nixpkgs ? ( import ../../inputs ).nixpkgs.flake
+, lib     ? import ../../lib { inherit (nixpkgs) lib; }
+, system  ? builtins.currentSystem
+, pkgsFor ? nixpkgs.legacyPackages.${system}
+, config  ? {
+    imports = [../../modules/top];
+    config._module.args.pkgs = pkgsFor;
+  }
+}: let
+
+# ---------------------------------------------------------------------------- #
+
+  floco = lib.evalModules { modules = [config ./floco.nix]; };
+
+# ---------------------------------------------------------------------------- #
+
+in floco.config.flocoPackages.packages."@npmcli/arborist"."6.1.5".global
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
