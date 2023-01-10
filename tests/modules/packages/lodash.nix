b1dd13881e9fb128f4b23040a523ffa84c13ea32
aameen-tulip <98349328+aameen-tulip@users.noreply.github.com>
2023-01-10 01:04:34 -0600

    Update lodash.nix

--- tests/modules/packages/lodash.nix
+++ tests/modules/packages/lodash.nix
@@ -1,12 +1,8 @@
 # ============================================================================ #
 #
-# Tests evaluation of default package set, which ( at time of writing ) just
-# contains `lodash@4.17.21'.
+# Tests evaluation of a package set, which just contains `lodash@4.17.21'.
 #
 # This essentially exists to force evaluation of all submodules.
-#
-# XXX: In the future if the default package set is removed, you can simply
-# inline the old declaration here.
 #
 # Run with:  nix eval --impure --json -f ./lodash.nix --apply 'f: f {}';
 #
