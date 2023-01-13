254f44df47b46f36febb351fc2414ebd7c4695f1
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-13 17:24:43 -0600

    fix priority of ident and version for pdefs

--- modules/pdefs/implementation.nix
+++ modules/pdefs/implementation.nix
@@ -21,9 +21,15 @@ in {
         ( { options, ... }: let
           inherit (options.key) loc;
         in {
-          config.ident   = lib.mkOverride 10 ( builtins.elemAt loc oloc );
+          # Priority prefers low numbers - "low priority" means "big number",
+          # "high priority" means "low number".
+          # The lowest priority is 1500 which is used by
+          # `lib.mkOption { default = <VAL>; }', followed by `lib.mkDefault'
+          # which uses 1000.
+          # We want this fallback to float around the low end so 1400 is fine.
+          config.ident   = lib.mkOverride 1400 ( builtins.elemAt loc oloc );
           config.version =
-            lib.mkOverride 10 ( builtins.elemAt loc ( oloc + 1 ) );
+            lib.mkOverride 1400 ( builtins.elemAt loc ( oloc + 1 ) );
         } )
         ../pdef/implementation.nix
       ];
