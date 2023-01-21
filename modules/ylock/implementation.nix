0d5c1445930ea9deb38e73b91c608b4c195e32b6
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-21 16:05:46 -0600

    convert yarn.lock to json and load it to ylents

--- modules/ylock/implementation.nix
+++ modules/ylock/implementation.nix
@@ -5,11 +5,12 @@
 # ---------------------------------------------------------------------------- #
 
 { lib
+, pkgs
 , config
 , lockDir
 , ylock   ? null
 , ...
-}: let
+} @ args: let
 
 # ---------------------------------------------------------------------------- #
 
@@ -17,7 +18,37 @@
 
 # ---------------------------------------------------------------------------- #
 
+  ylock = let
+    drv = derivation {
+      name = "yarn-lock.json";
+      src  = builtins.path {
+        path      = lockDir + "/yarn.lock";
+        recursive = false;
+      };
+      PATH    = "${pkgs.yaml2json}/bin";
+      builder = "${pkgs.bash}/bin/bash";
+      args    = ["-eu" "-o" "pipefail" "-c" ''
+        yaml2json < "$src" > "$out";
+      ''];
+      inherit (pkgs) system;
+      preferLocalBuild = true;
+      allowSubstitutes = ( builtins.currentSystem or "unknown" ) != pkgs.system;
+    };
+  in if ( args.ylock or null ) != null then args.ylock else
+     lib.importJSON drv.outPath;
 
+
+# ---------------------------------------------------------------------------- #
+
+  # FIXME: `version: 0.0.0-use.local' gets written for workspace members.
+  mkEnt = descs: config: let
+    m     = builtins.match "((@[^@/]+/)?[^@/]+)@.*" config.resolution;
+    ident = builtins.head m;
+  in {
+    inherit ident;
+    key         = ident + "/" + config.version;
+    descriptors = lib.splitString ", " descs;
+  } // config;
 
 
 # ---------------------------------------------------------------------------- #
@@ -30,6 +61,12 @@ in {
 
 # ---------------------------------------------------------------------------- #
 
+  config = {
+    _module.args.ylock = lib.mkDefault null;
+    inherit lockDir ylock;
+    lockfileVersion = ylock.__metadata.version;
+    ylents = builtins.mapAttrs mkEnt ( removeAttrs ylock ["__metadata"] );
+  };
 
 
 # ---------------------------------------------------------------------------- #
