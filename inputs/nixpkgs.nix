915ee869d65e395a9d24b2a6dddd0b0402cd27be
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-24 13:43:19 -0600

    fix nixpkgs pin in inputs file

--- inputs/nixpkgs.nix
+++ inputs/nixpkgs.nix
@@ -1,6 +1,6 @@
 let
   lock = builtins.fromJSON ( builtins.readFile ../flake.lock );
-  nl   = lock.nodes.nixpkgs.locked;
+  nl   = lock.nodes.nixpkgs_2.locked;
   uri  = nl.type + ":" + nl.owner + "/" + nl.repo + "/" + nl.rev;
 in {
   inherit uri;
