22ed2933ce9be1b1099b64647a1e5ab8b7a39bfe
Alex Ameen <alex.ameen.tx@gmail.com>
2023-01-31 08:15:12 -0600

    update semver and which defs

--- fpkgs/semver/default.nix
+++ fpkgs/semver/default.nix
@@ -8,15 +8,17 @@
 , lib     ? import ../../lib { inherit (nixpkgs) lib; }
 , system  ? builtins.currentSystem
 , pkgsFor ? nixpkgs.legacyPackages.${system}
-, fcfg    ? {
-    imports = [../../modules/top];
-    config._module.args.pkgs = pkgsFor;
-  }
 }: let
 
 # ---------------------------------------------------------------------------- #
 
-  fmod = lib.evalModules { modules = [fcfg ./floco.nix]; };
+  fmod = lib.evalModules {
+    modules = [
+      ../../modules/top
+      { config._module.args.pkgs = pkgsFor; }
+      ./floco-cfg.nix
+    ];
+  };
 
 # ---------------------------------------------------------------------------- #
 
--- fpkgs/semver/pdefs.nix
+++ fpkgs/semver/pdefs.nix
@@ -1,58 +1,64 @@
-[
-  {
-    binInfo = {
-      binPairs = {
-        semver = "bin/semver.js";
+{
+  floco.pdefs = {
+
+    semver."7.3.8" = {
+      binInfo = {
+        binPairs = {
+          semver = "bin/semver.js";
+        };
       };
-    };
-    depInfo = {
-      lru-cache = {
-        descriptor = "^6.0.0";
-        runtime = true;
+      depInfo = {
+        lru-cache = {
+          descriptor = "^6.0.0";
+          runtime = true;
+        };
       };
-    };
-    fetchInfo = {
-      narHash = "sha256-vqtjrIFs0Yw18hcdfShdL7BwyzqXdZ+K60Rp3oLNo/A="; 
-      type = "tarball";
-      url = "https://registry.npmjs.org/semver/-/semver-7.3.8.tgz";
-    };
-    ident = "semver";
-    ltype = "dir";
-    treeInfo = {
-      "node_modules/lru-cache" = {
-        key = "lru-cache/6.0.0";
+      fetchInfo = {
+        narHash = "sha256-vqtjrIFs0Yw18hcdfShdL7BwyzqXdZ+K60Rp3oLNo/A=";
+        type = "tarball";
+        url = "https://registry.npmjs.org/semver/-/semver-7.3.8.tgz";
       };
-      "node_modules/lru-cache/node_modules/yallist" = {
-        key = "yallist/4.0.0";
+      ident = "semver";
+      ltype = "file";
+      treeInfo = {
+        "node_modules/lru-cache" = {
+          key = "lru-cache/6.0.0";
+        };
+        "node_modules/lru-cache/node_modules/yallist" = {
+          key = "yallist/4.0.0";
+        };
       };
+      version = "7.3.8";
     };
-    version = "7.3.8";
-  }
-  {
-    depInfo = {
-      yallist = {
-        descriptor = "^4.0.0";
-        runtime = true;
+
+    lru-cache."6.0.0" = {
+      depInfo = {
+        yallist = {
+          descriptor = "^4.0.0";
+          runtime = true;
+        };
       };
+      fetchInfo = {
+        narHash = "sha256-lBc6340YZYAh1Numj5iz418ChtGb3UUtRZLOYj/WJXg=";
+        type = "tarball";
+        url = "https://registry.npmjs.org/lru-cache/-/lru-cache-6.0.0.tgz";
+      };
+      ident = "lru-cache";
+      ltype = "file";
+      version = "6.0.0";
     };
-    fetchInfo = {
-      narHash = "sha256-lBc6340YZYAh1Numj5iz418ChtGb3UUtRZLOYj/WJXg=";
-      type = "tarball";
-      url = "https://registry.npmjs.org/lru-cache/-/lru-cache-6.0.0.tgz";
+
+    yallist."4.0.0" = {
+      fetchInfo = {
+        narHash = "sha256-JQNNkqswg1ZH4o8PQS2R8WsZWJtv/5R3vRgc4d1vDR0=";
+        type = "tarball";
+        url = "https://registry.npmjs.org/yallist/-/yallist-4.0.0.tgz";
+      };
+      ident = "yallist";
+      ltype = "file";
+      treeInfo = { };
+      version = "4.0.0";
     };
-    ident = "lru-cache";
-    ltype = "file";
-    version = "6.0.0";
-  }
-  {
-    fetchInfo = {
-      narHash = "sha256-JQNNkqswg1ZH4o8PQS2R8WsZWJtv/5R3vRgc4d1vDR0=";
-      type = "tarball";
-      url = "https://registry.npmjs.org/yallist/-/yallist-4.0.0.tgz";
-    };
-    ident = "yallist";
-    ltype = "file";
-    treeInfo = { };
-    version = "4.0.0";
-  }
-]
+
+  };
+}
