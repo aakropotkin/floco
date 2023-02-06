880d4ae80192040c9159df7518a0b92accbe0368
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-06 09:25:07 -0600

    helper functions for ParsedSpec

--- pkgs/nix-plugin/parse.cc
+++ pkgs/nix-plugin/parse.cc
@@ -62,205 +62,257 @@ isExactVersion( const std::string & v )
 
 /* -------------------------------------------------------------------------- */
 
-  ParsedSpec::ParsedSpec( const std::string & raw )
-  {
-    std::string unesc = std::regex_replace( raw, std::regex( "%40" ), "@" );
-    unesc = std::regex_replace( unesc, std::regex( "\%2[fF]" ), "/" );
-    auto path = tokenizeString<std::vector<std::string>>( unesc, "/" );
-
-    auto size = path.size();
-    /**
-     * @foo/bar/1.0.0
-     * baz/1.0.0
-     * %40foo%2Fbar/1.0.0
-     * @foo%2fbar/1.0.0
-     */
-    if ( size == 3 )
-      {
-        if ( std::regex_match( path[0], npmUnescapedScopeRegex ) &&
-             std::regex_match( path[1], npmUnescapedBnameRegex )
-           )
-          {
-            this->scope   = path[0].substr( 1 );
-            this->bname   = path[1];
-            this->locator = path[2];
-          }
-      }
-    else if ( size == 2 )
-      {
-        auto spat =
-          tokenizeString<std::vector<std::string>>( path[1], "@" );
-        /* baz/1.0.0, %40foo%2Fbar/1.0.0, @foo%2fbar/1.0.0 */
-        if ( std::regex_match( path[0], npmUnescapedBnameRegex ) )
-          {
-            this->scope   = std::nullopt;
-            this->bname   = path[0];
-            this->locator = path[1];
-          }
-        else if ( std::regex_match( path[0], npmUnescapedScopeRegex ) &&
-                  std::regex_match( spat[0], npmUnescapedBnameRegex )
-                )
-          {
-            this->scope   = path[0].substr( 1 );
-            this->bname   = path[1];
-            this->locator = path[2];
-          }
-        else
-          {
-            throw std::runtime_error(
-              "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
-            );
-          }
-      }
-    else if ( size == 1 )
-      {
-        auto spat =
-          tokenizeString<std::vector<std::string>>( path[0], "@" );
-        if ( std::regex_match( spat[0], npmUnescapedBnameRegex ) )
-          {
-            this->scope   = std::nullopt;
-            this->bname   = spat[0];
-            this->locator = spat[1];
-          }
-        else
-          {
-            throw std::runtime_error(
-              "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
-            );
-          }
-      }
-    else
-      {
-        throw std::runtime_error(
-          "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
-        );
-      }
-
-    if ( isExactVersion( this->locator ) )
-      {
-        this->kind = SpecKind::version;
-      }
-    else
-      {
-        if ( ( strchr( this->locator.c_str(), ':' ) != NULL ) ||
-             ( strchr( this->locator.c_str(), ':' ) != NULL )
-           )
-          {
-            this->kind = SpecKind::url;
-          }
-        else
-          {
-            this->kind = SpecKind::semver;
-          }
-      }
-  }
+  void
+ParsedSpec::setKind()
+{
+  if ( this->locator.has_value() )
+    {
+      if ( isExactVersion( this->locator.value() ) )
+        {
+          this->kind = SpecKind::version;
+        }
+      else
+        {
+          // Assume that anything containing `[:/]' is a URL.
+          // TODO: Write a real REGEX pattern to detect `SemVer' properly.
+          if ( ( strchr( this->locator.value().c_str(), ':' ) != NULL ) ||
+               ( strchr( this->locator.value().c_str(), '/' ) != NULL )
+             )
+            {
+              this->kind = SpecKind::url;
+            }
+          else
+            {
+              this->kind = SpecKind::semver;
+            }
+        }
+    }
+  else
+    {
+      this->kind = SpecKind::nullspec;
+    }
+}
 
 
 /* -------------------------------------------------------------------------- */
 
-  ParsedSpec::ParsedSpec(
-    const std::string & ident,
-    const std::string & locator
-  )
-  {
-    if ( ident[0] == '@' )
-      {
-        int i = strcspn( ident.c_str(), "/" );
-        this->scope   = ident.substr( 1, i - 1 );
-        this->bname   = ident.substr( i + 1 );
-        this->locator = locator;
-      }
-    else
-      {
-        this->scope   = std::nullopt;
-        this->bname   = ident;
-        this->locator = locator;
-      }
-
-    if ( isExactVersion( locator ) )
-      {
-        this->kind = SpecKind::version;
-      }
-    else
-      {
-        if ( ( strchr( locator.c_str(), ':' ) != NULL ) ||
-             ( strchr( locator.c_str(), ':' ) != NULL )
-           )
-          {
-            this->kind = SpecKind::url;
-          }
-        else
-          {
-            this->kind = SpecKind::semver;
-          }
-      }
-  }
+  void
+ParsedSpec::initIdent( const std::string & ident )
+{
+  /* Scoped */
+  if ( ( ident[0] == '@' ) || ( ident[0] == '%' ) )
+    {
+      if ( ident[0] == '@' )
+        {
+          int i = strcspn( ident.c_str(), "/" );
+          this->scope = ident.substr( 1, i - 1 );
+          this->bname = ident.substr( i + 1 );
+        }
+      else
+        {
+          this->scope = ident.substr( 3 );
+          int i = strcspn( this->scope.value().c_str(), "%/" );
+          if ( this->scope.value()[i] == '%' )
+            {
+              this->bname = this->scope.value().substr( i + 3 );
+            }
+          else
+            {
+              this->bname = this->scope.value().substr( i + 1 );
+            }
+          this->scope = this->scope.value().substr( 0, i - 1 );
+        }
+    }
+  else
+    {
+      this->scope = std::nullopt;
+      this->bname = ident;
+    }
+}
 
 
 /* -------------------------------------------------------------------------- */
 
-  ParsedSpec::ParsedSpec(
-    const std::optional<std::string> & scope,
-    const std::string                & bname,
-    const std::string                & locator
-  )
-  {
-    this->bname   = bname;
-    this->locator = locator;
+ParsedSpec::ParsedSpec( const std::string & raw )
+{
+  std::string unesc = std::regex_replace( raw, std::regex( "%40" ), "@" );
+  unesc = std::regex_replace( unesc, std::regex( "\%2[fF]" ), "/" );
+  auto path = tokenizeString<std::vector<std::string>>( unesc, "/" );
 
-    /* Handle scope part */
-    if ( scope.has_value() )
-      {
-        if ( scope.value()[0] == '@' )
-          {
-            if ( scope.value().back() == '/' )
-              {
-                this->scope =
-                  scope.value().substr( 1, scope.value().size() - 1 );
-              }
-            else
-              {
-                this->scope = scope.value().substr( 1 );
-              }
-          }
-        else
-          {
-            if ( scope.value().back() == '/' )
-              {
-                this->scope =
-                  scope.value().substr( 0, scope.value().size() - 1 );
-              }
-            else
-              {
-                this->scope = scope;
-              }
-          }
-      }
-    else
-      {
-        this->scope = scope;
-      }
+  auto size = path.size();
+  /**
+   * @foo/bar/1.0.0
+   * baz/1.0.0
+   * %40foo%2Fbar/1.0.0
+   * @foo%2fbar/1.0.0
+   */
+  if ( size == 3 )
+    {
+      if ( std::regex_match( path[0], npmUnescapedScopeRegex ) &&
+           std::regex_match( path[1], npmUnescapedBnameRegex )
+         )
+        {
+          this->scope   = path[0].substr( 1 );
+          this->bname   = path[1];
+          this->locator = path[2];
+        }
+    }
+  else if ( size == 2 )
+    {
+      auto spat =
+        tokenizeString<std::vector<std::string>>( path[1], "@" );
+      /* baz/1.0.0, %40foo%2Fbar/1.0.0, @foo%2fbar/1.0.0 */
+      if ( std::regex_match( path[0], npmUnescapedBnameRegex ) )
+        {
+          this->scope   = std::nullopt;
+          this->bname   = path[0];
+          this->locator = path[1];
+        }
+      else if ( std::regex_match( path[0], npmUnescapedScopeRegex ) &&
+                std::regex_match( spat[0], npmUnescapedBnameRegex )
+              )
+        {
+          this->scope   = path[0].substr( 1 );
+          this->bname   = path[1];
+          this->locator = path[2];
+        }
+      else
+        {
+          throw std::runtime_error(
+            "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
+          );
+        }
+    }
+  else if ( size == 1 )
+    {
+      auto spat =
+        tokenizeString<std::vector<std::string>>( path[0], "@" );
+      if ( std::regex_match( spat[0], npmUnescapedBnameRegex ) )
+        {
+          this->scope   = std::nullopt;
+          this->bname   = spat[0];
+          this->locator = spat[1];
+        }
+      else
+        {
+          throw std::runtime_error(
+            "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
+          );
+        }
+    }
+  else
+    {
+      throw std::runtime_error(
+        "'" + raw + "' is not a valid <SCOPE>[@/]<VERSION> URI"
+      );
+    }
 
-    /* Handle locator part */
-    if ( isExactVersion( locator ) )
-      {
-        this->kind = SpecKind::version;
-      }
-    else
-      {
-        if ( ( strchr( locator.c_str(), ':' ) != NULL ) ||
-             ( strchr( locator.c_str(), ':' ) != NULL )
-           )
-          {
-            this->kind = SpecKind::url;
-          }
-        else
-          {
-            this->kind = SpecKind::semver;
-          }
-      }
+  if ( isExactVersion( this->locator ) )
+    {
+      this->kind = SpecKind::version;
+    }
+  else
+    {
+      if ( ( strchr( this->locator.c_str(), ':' ) != NULL ) ||
+           ( strchr( this->locator.c_str(), ':' ) != NULL )
+         )
+        {
+          this->kind = SpecKind::url;
+        }
+      else
+        {
+          this->kind = SpecKind::semver;
+        }
+    }
+}
 
-  }
+
+/* -------------------------------------------------------------------------- */
+
+ParsedSpec::ParsedSpec(
+  const std::string & ident,
+  const std::string & locator
+)
+{
+  if ( ident[0] == '@' )
+    {
+      int i = strcspn( ident.c_str(), "/" );
+      this->scope   = ident.substr( 1, i - 1 );
+      this->bname   = ident.substr( i + 1 );
+      this->locator = locator;
+    }
+  else
+    {
+      this->scope   = std::nullopt;
+      this->bname   = ident;
+      this->locator = locator;
+    }
+
+  if ( isExactVersion( locator ) )
+    {
+      this->kind = SpecKind::version;
+    }
+  else
+    {
+      if ( ( strchr( locator.c_str(), ':' ) != NULL ) ||
+           ( strchr( locator.c_str(), '/' ) != NULL )
+         )
+        {
+          this->kind = SpecKind::url;
+        }
+      else
+        {
+          this->kind = SpecKind::semver;
+        }
+    }
+}
+
+
+/* -------------------------------------------------------------------------- */
+
+ParsedSpec::ParsedSpec(
+  const std::optional<std::string> & scope,
+  const std::string                & bname,
+  const std::string                & locator
+)
+{
+  this->bname   = bname;
+  this->locator = locator;
+
+  /* Handle scope part */
+  if ( scope.has_value() )
+    {
+      if ( scope.value()[0] == '@' )
+        {
+          if ( scope.value().back() == '/' )
+            {
+              this->scope =
+                scope.value().substr( 1, scope.value().size() - 1 );
+            }
+          else
+            {
+              this->scope = scope.value().substr( 1 );
+            }
+        }
+      else
+        {
+          if ( scope.value().back() == '/' )
+            {
+              this->scope =
+                scope.value().substr( 0, scope.value().size() - 1 );
+            }
+          else
+            {
+              this->scope = scope;
+            }
+        }
+    }
+  else
+    {
+      this->scope = scope;
+    }
+
+}
 
 
 
--- pkgs/nix-plugin/parse.hh
+++ pkgs/nix-plugin/parse.hh
@@ -6,6 +6,7 @@
 
 #include <nix/fetchers.hh>
 #include <nix/url-parts.hh>
+#include <stdexcept>
 #include <optional>
 #include <iostream>
 
@@ -16,23 +17,61 @@ namespace floco::parse {
 
 /* -------------------------------------------------------------------------- */
 
-  typedef enum { semver, version, url } SpecKind;
+  typedef enum { semver, version, url, nullspec } SpecKind;
 
-  struct ParsedSpec {
+    static std::string
+  specKindToString( const SpecKind & kind )
+  {
+    switch ( kind )
+    {
+      case semver:   return "SemVer";  break;
+      case version:  return "Version"; break;
+      case url:      return "URL";     break;
+      case nullspec: return "NULL";    break;
+      default:
+        throw std::invalid_argument( "Unrecognized SpecKind" );
+        break;
+    }
+  }
 
-    std::optional<std::string> scope;
-    std::string                bname;
-    std::string                locator;
-    SpecKind                   kind;
+
+/* -------------------------------------------------------------------------- */
+
+  class ParsedSpec {
+
+    private:
+      void setKind();
+      void initIdent( const std::string & ident );
+
+    public:
+      std::optional<std::string> scope;
+      std::string                bname;
+      std::optional<std::string> locator;
+      SpecKind                   kind;
 
     ParsedSpec( const std::string & raw );
-    ParsedSpec( const std::string & ident, const std::string & locator );
+    ParsedSpec( const std::string & ident,
+                const std::optional<std::string> & locator
+              );
     ParsedSpec( const std::optional<std::string> & scope,
                 const std::string                & bname,
-                const std::string                & locator
+                const std::optional<std::string> & locator
               );
 
     bool isScoped() const { return this->scope.has_value(); }
+
+      std::string
+    scopeDir() const
+    {
+      if ( this->isScoped() )
+        {
+          return "@" + this->scope.value() + "/";
+        }
+      else
+        {
+          return "";
+        }
+    }
 
       std::string
     ident() const
@@ -47,22 +86,27 @@ namespace floco::parse {
         }
     }
 
-    std::string toString() const { return this->ident() + "@" + this->locator; }
+    std::string toString() const {
+      if ( this->locator.has_value() )
+        {
+          return this->ident() + "@" + this->locator.value();
+        }
+      else
+        {
+          return this->ident();
+        }
+    }
 
       void
     show() const
     {
-      const std::string k(
-        this->kind == SpecKind::version ? "Version" :
-        this->kind == SpecKind::semver  ? "SemVer"  : "URL"
-      );
       std::cerr << "{\n  scope:   " + this->scope.value_or( "NULL" ) +
                    ",\n  bname:   " + this->bname +
-                   ",\n  locator: " + this->locator +
-                   ",\n  kind:    " + k + "\n}\n";
+                   ",\n  locator: " + this->locator.value_or( "NULL" ) +
+                   ",\n  kind:    " + specKindToString( this->kind ) + "\n}\n";
     }
 
-  };  /* End `struct ParsedSpec' */
+  };  /* End `class ParsedSpec' */
 
 
 /* -------------------------------------------------------------------------- */
