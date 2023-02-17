c8b8105e422dddaecbaa0ad5bf702abdcdda5408
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-17 12:33:18 -0600

    port inputScheme and input from Nix

--- lib/types.nix
+++ lib/types.nix
@@ -12,14 +12,18 @@
 
 # ---------------------------------------------------------------------------- #
 
-  jsonAtom = nt.nullOr ( nt.oneOf [nt.str nt.bool nt.int nt.float] );
+  jsonAtom = ( nt.nullOr ( nt.oneOf [nt.str nt.bool nt.int nt.float] ) ) // {
+    name        = "JSON atom";
+    description = "JSON `null`, `string`, `bool`, or `number` value";
+  };
 
   jsonValue = ( nt.oneOf [
     lib.libfloco.jsonAtom
     ( nt.listOf jsonValue )
     ( nt.attrsOf jsonValue )
   ] ) // {
-    description = "A JSON compliant value";
+    name        = "JSON value";
+    description = "JSON compatible value";
   };
 
 
@@ -34,33 +38,50 @@
     tag_p     = "${part_p}(\\.${part_p})*";
     build_p   = "${dan_c}+(\\.[[:alnum:]]+)*";
     version_p = "${core_p}(-${tag_p})?(\\+${build_p})?";
-  in nt.strMatching version_p;
+  in ( nt.strMatching version_p ) // {
+    name        = "version";
+    description = "semantic version number";
+  };
 
 
 # ---------------------------------------------------------------------------- #
 
-  uri = nt.str;
+  uri = nt.str // {
+    name        = "URI";
+    description = "uniform resource identifier";
+  };
 
 
 # ---------------------------------------------------------------------------- #
 
-  descriptor = nt.either lib.libfloco.version lib.libfloco.uri;
+  descriptor = ( nt.either lib.libfloco.version lib.libfloco.uri ) // {
+    name        = "package descriptor";
+    description = "version or URI";
+  };
 
 
 # ---------------------------------------------------------------------------- #
 
-  ident = nt.strMatching "(@[^@/]+/)?[^@/]+";
+  ident = ( nt.strMatching "(@[^@/]+/)?[^@/]+" ) // {
+    name        = "package identifier";
+    description = "package identifier/name";
+  };
 
 
 # ---------------------------------------------------------------------------- #
 
-  key = nt.str;
+  key = nt.str // {
+    name        = "package key";
+    description = "unique package identifier";
+  };
 
 
 # ---------------------------------------------------------------------------- #
 
   ltype = ( nt.enum ["file" "link" "dir" "git"] ) // {
-    merge = lib.libfloco.mergePreferredOption {
+    name        = "lifecycle type";
+    description = "lifecycle type as recognized by `npm`";
+    merge       = lib.libfloco.mergePreferredOption {
       compare = a: b:
         if a == "file" then true else if b == "file" then false else
         if a == "dir"  then true else if b == "dir"  then false else
@@ -79,8 +100,240 @@
 
 # ---------------------------------------------------------------------------- #
 
-  binPairs = nt.attrsOf nt.str;
-  pjsBin   = nt.either nt.str lib.libfloco.binPairs;
+  relpath = ( nt.strMatching "[^/[:space:]].*" ) // {
+    name        = "relative path";
+    description = "relative path without leading slash";
+  };
+
+  storePath = ( nt.strMatching ( builtins.storeDir + "/.*" ) ) // {
+    name        = "nix store path";
+    description = "path to a file/directory in the nix store";
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  binPairs = nt.attrsOf lib.libfloco.relpath;
+  pjsBin   = nt.either lib.libfloco.relpath lib.libfloco.binPairs;
+
+
+# ---------------------------------------------------------------------------- #
+
+  sha256_hash = ( nt.strMatching "[[:xdigit:]]{64}" ) // {
+    name        = "SHA-256 hex";
+    description = "SHA-256 hash (hexadecimal)";
+  };
+  sha256_sri = ( nt.strMatching "sha256-[a-zA-Z0-9+/]{42,44}={0,2}" ) // {
+    name        = "SHA-256 SRI";
+    description = "SHA-256 hash (SRI)";
+  };
+  narHash = lib.libfloco.sha256_sri // {
+    name        = "narHash";
+    description = "NAR hash (SHA256 SRI)";
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  rev = ( nt.strMatching "[[:xdigit:]]{40}" ) // {
+    name        = "rev";
+    description = "SHA-1 revision (hexadecimal)";
+  };
+  short_rev = ( nt.strMatching "[[:xdigit:]]{7}" ) // {
+    name        = "short rev";
+    description = "first 7 characters of SHA-1 revision (hexadecimal)";
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  tree = nt.submodule {
+    options = {
+      storePath  = lib.mkOption {
+        description = "path to fetched tree in the nix store";
+        type        = lib.libfloco.storePath;
+      };
+      actualPath = lib.mkOption {
+        description = "original path outside of the nix store (if any)";
+        type        = nt.nullOr nt.path;
+        default     = null;
+      };
+    };
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  inputScheme = nt.submodule {
+    options = {
+
+      name = lib.mkOption {
+        description = lib.mdDoc ''
+          unique name of the input scheme used to refer to this input scheme in
+          `input` records.
+        '';
+        type = nt.str;
+      };
+
+      inputFromURL = lib.mkOption {
+        description = lib.mdDoc ''
+          Function which attempts to parse a URL into an `input`.
+
+          May return `null` if the URL is not recognized by this input scheme
+          or is otherwise unfetchable.
+          This allows other input schemes to be tried.
+       '';
+        type = nt.nullOr ( nt.functionTo ( nt.nullOr lib.libfloco.input ) );
+      };
+
+      inputFromAttrs = lib.mkOption {
+        description = lib.mdDoc ''
+          Function which attempts to transform an attrset into an `input`.
+
+          May return `null` if the attrsets' contents are not recognized by this
+          input scheme or is otherwise unfetchable.
+          This allows other input schemes to be tried.
+       '';
+        type = nt.nullOr ( nt.functionTo ( nt.nullOr lib.libfloco.input ) );
+      };
+
+      toURL = lib.mkOption { type = nt.functionTo lib.libfloco.uri; };
+
+      applyOverrides = lib.mkOption {
+        description = lib.mdDoc ''
+          Function which applies overrides to the input.
+
+          Overrides are an attrset of `{ input, rev ? null, ref ? null }` which
+          may be applied to a "base" input.
+          This behaves like Nix's input overrides such as `nixpkgs/<REV>`, where
+          `nixpkgs` is registered as an input and `<REV>` is an override used to
+          fetch a specific revision of `nixpkgs`.
+        '';
+        type    = nt.functionTo lib.libfloco.input;
+        default = { input, rev ? null, ref ? null }: input;
+      };
+
+      getSourcePath = lib.mkOption {
+        description = lib.mdDoc ''
+          Function which may return the original path of the input outside of
+          the nix store (if any).
+
+          This function may return `null` if there is not a non-store path form
+          of the input.
+
+          This function largely exists for working with local files/trees.
+        '';
+        type    = nt.functionTo ( nt.nullOr nt.path );
+        default = _: null;
+      };
+
+      fetch = lib.mkOption {
+        description = lib.mdDoc ''
+          Function which fetches an input.
+
+          Returns a pair `{ tree, input }` where `tree` is an attrset with the
+          attribute `outPath` ( being a nix store path ), and `input` is the
+          locked form of the given `input`.
+
+          Note that the `tree` attrset is not a `lib.libfloco.tree` submodule,
+          and may also contain arbitrary attributes.
+        '';
+        type = nt.functionTo ( nt.submodule {
+          options.tree  = lib.mkOption { type = nt.package; };
+          options.input = lib.mkOption { type = lib.libfloco.input; };
+        } );
+      };
+
+    };  # End `inputScheme.option'
+  };  # End `inputScheme'`
+
+
+
+# ---------------------------------------------------------------------------- #
+
+  input = nt.submodule {
+    options = {
+
+      schemeName = lib.mkOption { type = nt.nullOr nt.str; };
+      #scheme     = lib.mkOption { type = nt.nullOr lib.libfloco.inputScheme; };
+      scheme     = lib.mkOption { type = nt.nullOr nt.raw; };
+      attrs      = lib.mkOption { type = nt.attrsOf nt.raw; };
+      parent     = lib.mkOption { type = nt.nullOr nt.path; };
+
+      functor = lib.mkOption {
+        type = nt.submodule {
+          options = {
+
+            fromURL   = lib.mkOption { type = nt.functionTo nt.raw; };
+            fromAttrs = lib.mkOption { type = nt.functionTo nt.raw; };
+            toURL     = lib.mkOption { type = nt.functionTo nt.str; };
+            toAttrs   = lib.mkOption { type = nt.functionTo nt.raw; };
+            isLocked  = lib.mkOption {
+              type = nt.functionTo nt.bool;
+              default = i:
+                ( ( i.attrs.narHash or null ) != null ) ||
+                ( ( i.attrs.rev or null ) != null );
+            };
+
+            isDirect = lib.mkOption {
+              type = nt.functionTo nt.bool;
+              default = _: true;
+            };
+
+            getType = lib.mkOption {
+              type    = nt.functionTo nt.str;
+              default = i: i.scheme.type or i.schemeName;
+            };
+
+            getNarHash = lib.mkOption {
+              type    = nt.functionTo ( nt.nullOr lib.libfloco.narHash );
+              default = i: i.attrs.narHash or null;
+            };
+
+            getRev = lib.mkOption {
+              type    = nt.functionTo ( nt.nullOr lib.libfloco.rev );
+              default = i: i.attrs.rev or null;
+            };
+
+            getRef = lib.mkOption {
+              type    = nt.functionTo ( nt.nullOr nt.str );
+              default = i: i.attrs.ref or null;
+            };
+
+            fetch = lib.mkOption {
+              type = nt.functionTo ( nt.submodule {
+                options.tree   = lib.mkOption { type = lib.libfloco.tree; };
+                options.locked = lib.mkOption { type = lib.libfloco.input; };
+                freeformType   = nt.attrsOf nt.raw;
+              } );
+              default = input: let
+                fetched = input.scheme.fetch input;
+              in {
+                tree.actualPath = input.scheme.getSourcePath input;
+                tree.storePath  = fetched.tree.outPath;
+                locked          = fetched.input;
+              };
+            };
+
+            applyOverrides = lib.mkOption {
+              description = lib.mdDoc ''
+                Function which applies overrides to the input.
+
+                Overrides are an attrset of `{ input, rev ? null, ref ? null }`
+                which may be applied to a "base" input.
+                This behaves like Nix's input overrides such as `nixpkgs/<REV>`,
+                where `nixpkgs` is registered as an input and `<REV>` is an
+                override used to fetch a specific revision of `nixpkgs`.
+              '';
+              type    = nt.functionTo lib.libfloco.input;
+              default = { input, rev ? null, ref ? null }: input;
+            };
+
+          };  # End `input.options.functor.type.options'
+        };  # End `input.options.functor.type'
+      };  # End `input.options.functor'
+    };  # End `input.options'
+  };  # End `input'
 
 
 # ---------------------------------------------------------------------------- #
@@ -88,21 +341,22 @@
 in {
 
   inherit
-    jsonAtom
-    jsonValue
+    jsonAtom jsonValue
 
-    version
     uri
-    descriptor
-    ident
-    key
+
+    version descriptor
+    ident key
     ltype
+    depAttrs depMetas
+    binPairs pjsBin
 
-    depAttrs
-    depMetas
+    relpath storePath
 
-    binPairs
-    pjsBin
+    sha256_hash sha256_sri narHash
+    rev short_rev
+
+    tree inputScheme input
   ;
 
 }
---
+++ tests/inputs/tb-scheme/scheme.nix
@@ -0,0 +1,108 @@
+# ============================================================================ #
+#
+#
+#
+# ---------------------------------------------------------------------------- #
+
+{ lib, config, options, ... }: let
+
+# ---------------------------------------------------------------------------- #
+
+  nt = lib.types;
+  ft = lib.libfloco;
+
+# ---------------------------------------------------------------------------- #
+
+in {
+
+  options.tbScheme = lib.mkOption { type = lib.libfloco.inputScheme; };
+
+  config.tbScheme = let
+
+    mkInput = attrs: {
+      schemeName = lib.mkDerivedConfig options.tbScheme ( s: s.name );
+      scheme     = lib.mkDerivedConfig options.tbScheme lib.id;
+      attrs      = { type = "tarball"; } // attrs;
+      parent     = null;
+      functor    = { config, options, ... }: {
+        fromURL   = lib.mkDerivedConfig options.tbScheme ( s: s.inputFromURL );
+        fromAttrs =
+          lib.mkDerivedConfig options.tbScheme ( s: s.inputFromAttrs );
+        toURL   = lib.mkDerivedConfig options.tbScheme ( s: s.toURL );
+        toAttrs = lib.mkDerivedConfig options.tbScheme ( s: input:
+          ( s.fetch input ).input.attrs
+        );
+        isLocked   = input: ( input.attrs.narHash or null ) != null;
+        getType    = _: "tarball";
+        getNarHash = lib.mkDerivedConfig options.tbScheme ( s: input:
+          if input.isLocked input then input.attrs.narHash else
+          ( s.fetch input ).input.attrs.narHash
+        );
+        getRev         = _: null;
+        getRef         = _: null;
+        applyOverrides =
+          lib.mkDerivedConfig options.tbScheme ( s: s.applyOverrides );
+      };
+    };
+
+    inputFromURL = urlRaw: let
+      url' = lib.yankN 1 "(tarball[+:]?)?(.*)" urlRaw;
+      url  = let
+        n = builtins.match "(file:/?)?(/.*)" url';
+      in if n == null then url' else builtins.elemAt n 1;
+      m    = builtins.match ".*([&?](narHash|sha256)=([^&]+)).*" url;
+      nh   = if m == null then null else builtins.elemAt m 2;
+      nh'  = if nh == null then {} else { narHash = nh; };
+    in mkInput ( {
+      url = if m == null then url else
+            builtins.replaceStrings [( builtins.head m )] [
+              ( if ( ( builtins.substring 0 1 ( builtins.head m ) ) == "&" ) ||
+                   ( lib.hasSuffix nh url )
+                then ""
+                else "?" )
+            ] url;
+    } // nh' );
+
+  in {
+    name = "tarball";
+
+    inherit inputFromURL;
+
+    inputFromAttrs = attrs: let
+      nh  = attrs.narHash or attrs.sha256 or attrs.outputHash or null;
+      nh' = if nh == null then {} else { narHash = nh; };
+      fu  = inputFromURL attrs.url;
+    in mkInput ( fu.attrs // nh' );
+
+    toURL = input: let
+      pre = if lib.test "(file|https)://.*" input.attrs.url then "tarball+" else
+            "tarball:";
+    in
+      if ! input.isLocked input then pre + input.attrs.url else
+      if lib.test ".*?.*" input.attrs.url
+      then pre + input.attrs.url + "&narHash=" + input.attrs.narHash
+      else pre + input.attrs.url + "?narHash=" + input.attrs.narHash;
+
+    getSourcePath = input: let
+      p = lib.yank "(/[^?]*)(\\?.*)?" input.attrs.url;
+    in if p == null then null else /. + p;
+
+    fetch = input: let
+      sourceInfo = builtins.fetchTree input.attrs;
+    in {
+      tree  = sourceInfo;
+      input = input // {
+        attrs = { inherit (sourceInfo) narHash; } // input.attrs;
+      };
+    };
+
+  };  # End `config.tbScheme'
+
+}
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
