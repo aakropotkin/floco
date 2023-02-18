3ad4829b4557a89ee639c5b5b1840c5f59db2e1c
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-18 09:14:03 -0600

    semverRangeValid

--- pkgs/lib/default.nix
+++ pkgs/lib/default.nix
@@ -9,6 +9,7 @@
 , system  ? args.pkgsFor.system or builtins.currentSystem
 , pkgsFor ? nixpkgs.legacyPackages.${system}.extend ( import ../../overlay.nix )
 , semver  ? pkgsFor.semver
+, nodejs  ? pkgsFor.nodejs-14_x
 , treeFor ? pkgsFor.semver
 , bash    ? pkgsFor.bash
 , ...
@@ -16,6 +17,10 @@
 
   semverRangeFilt = range: versions: import ./semver.nix {
     inherit system bash semver range versions;
+  };
+
+  semverRangeValid = range: import ./semver/valid.nix {
+    inherit system bash semver nodejs range;
   };
 
   treeFor = src: import ./tree-for.nix {
