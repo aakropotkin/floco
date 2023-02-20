96dbe54fa0cd89411bfeca6ae3f1ad4c586a8c18
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-19 20:33:22 -0600

    cli helpers

---
+++ pkgs/cli/src/common.sh
@@ -0,0 +1,111 @@
+#! /usr/bin/env bash
+# ============================================================================ #
+#
+#
+#
+# ---------------------------------------------------------------------------- #
+
+set -eu;
+set -o pipefail;
+
+
+# ---------------------------------------------------------------------------- #
+
+: "${REALPATH:=realpath}";
+
+export REALPATH;
+
+# ---------------------------------------------------------------------------- #
+
+SPATH="$( $REALPATH "${BASH_SOURCE[1]}"; )";
+SDIR="${SPATH%/*}";
+_as_me="${SPATH##*/}";
+
+export SPATH SDIR _as_me;
+
+
+# ---------------------------------------------------------------------------- #
+
+# stopSearch DIR
+# --------------
+# Return 0 if DIR's parent is searchable, 1 otherwise.
+keepSearching() {
+  ! { [[ "$( $REALPATH "$1"; )" = '/' ]] || [[ -d "$1/.git" ]]; };
+}
+
+# searchUp FILE [DIR]
+searchUp() {
+  if [[ -r "${2:-$PWD}/$1" ]]; then
+    echo "$( $REALPATH "${2:-$PWD}/$1"; )";
+  elif keepSearching "${2:-$PWD}"; then
+    searchUp "$1" "${2:-$PWD}/..";
+  else
+    return 1;
+  fi
+}
+
+
+# ---------------------------------------------------------------------------- #
+
+if [[ -z "${_g_floco_cfg+y}" ]]; then
+  if [[ -r /etc/floco/floco-cfg.nix ]]; then
+    export _g_floco_cfg='/etc/floco/floco-cfg.nix';
+  elif [[ -r /etc/floco/floco-cfg.json ]]; then
+    export _g_floco_cfg='/etc/floco/floco-cfg.json';
+  else
+    export _g_floco_cfg=;
+  fi
+fi
+
+
+# ---------------------------------------------------------------------------- #
+
+: "${XDG_CONFIG_HOME:=${HOME:-/homeless/shelter}/.config}";
+export XDG_CONFIG_HOME;
+
+if [[ -z "${_u_floco_cfg+y}" ]]; then
+  if [[ -r $XDG_CONFIG_HOME/floco/floco-cfg.nix ]]; then
+    export _u_floco_cfg="$XDG_CONFIG_HOME/floco/floco-cfg.nix";
+  elif [[ -r $XDG_CONFIG_HOME/floco/floco-cfg.json ]]; then
+    export _u_floco_cfg="$XDG_CONFIG_HOME/floco/floco-cfg.json";
+  else
+    export _u_floco_cfg=;
+  fi
+fi
+
+
+# ---------------------------------------------------------------------------- #
+
+: "${_l_floco_cfg=}";
+export _l_floco_cfg;
+
+# Lazily locate the closest `floco-cfg.{json,nix}' between `PWD' and the closest
+# repository root, or filesystem root.
+localFlocoCfg() {
+  if [[ -z "${_l_floco_cfg:=$( searchUp floco-cfg.nix||:; )}" ]]; then
+    unset _floco_cfg;
+    if [[ -z "${_l_floco_cfg:=$( searchUp floco-cfg.json||:; )}" ]]; then
+      echo "$_as_me: no floco-cfg.nix or floco-cfg.json found" >&2;
+      return 1;
+    fi
+  fi
+  echo "$_l_floco_cfg";
+}
+
+
+# ---------------------------------------------------------------------------- #
+
+flocoCfgFiles() {
+  if [[ -n "$_g_floco_cfg" ]]; then echo "$_g_floco_cfg"; fi
+  if [[ -n "$_u_floco_cfg" ]]; then echo "$_u_floco_cfg"; fi
+  {
+    if [[ -n "$( localFlocoCfg||:; )" ]]; then echo "$_l_floco_cfg"; fi
+  } 2>/dev/null;
+}
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
---
+++ pkgs/cli/src/test-common.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+. "${BASH_SOURCE[0]%/*}/common.sh";
+
+echo "SPATH: $SPATH";
+echo "SDIR: $SDIR";
+echo "_as_me: $_as_me";
+
+flocoCfgFiles;
