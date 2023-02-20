80a87e9a191eed906e0b00d8d6fab7dbf00b008a
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-20 17:09:00 -0600

    add floco show subcommand

--- pkgs/cli/src/show/show-pdefs.sh
+++ pkgs/cli/src/show/show-pdefs.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 # ============================================================================ #
 #
-#
+# List all known packages declared in a given directory.
 #
 # ---------------------------------------------------------------------------- #
 
@@ -11,22 +11,24 @@ set -o pipefail;
 
 # ---------------------------------------------------------------------------- #
 
-_as_me="floco";
+_as_me="floco show";
 
-_version="0.1.0";
+: "${_version:=0.1.0}";
 
-_usage_msg="USAGE: $_as_me [OPTIONS...] CMD [ARGS...];
+_usage_msg="$_as_me [OPTIONS...] IDENT[@|/]VERSION [-- FLOCO-CMD-ARGS...]
 
-COMMANDS
-  list              List available packages.
-  show KEY          Show declared package definition ( \`pdef' ).
-  help CMD          Show help for \`CMD'.
+Show a package definition ( \`pdef' record ).
 ";
 
 _help_msg="$_usage_msg
-Run \`floco help CMD' for help on a specific command.
+This list will include any \"global\" or \"user\" level declarations made in
+associated \`floco-cfg.{nix,json}' files if they exis.
+With that in mind, you may wish to avoid creating such declarations in
+global/user configs, or setting the ENV vars \`_[gu]_floco_cfg=null'.
 
 OPTIONS
+  -m,--more         Show more details.
+  -j,--json         Output a JSON list.
   -h,--help         Print help message to STDOUT.
   -u,--usage        Print usage message to STDOUT.
   -v,--version      Print version information to STDOUT.
@@ -59,18 +61,18 @@ usage() {
 
 # @BEGIN_INJECT_UTILS@
 : "${GREP:=grep}";
-: "${HEAD:=head}";
-: "${JQ:=jq}";
+: "${REALPATH:=realpath}";
 : "${MKTEMP:=mktemp}";
 : "${NIX:=nix}";
-: "${REALPATH:=realpath}";
-
-export GREP HEAD JQ MKTEMP NIX REALPATH;
-
-. "${BASH_SOURCE[0]%/*}/common.sh";
+: "${JQ:=jq}";
+: "${HEAD:=head}";
 
 
 # ---------------------------------------------------------------------------- #
+
+unset _IDENT _VERSION;
+_MORE=;
+_JSON=;
 
 while [[ "$#" -gt 0 ]]; do
   case "$1" in
@@ -88,60 +90,50 @@ while [[ "$#" -gt 0 ]]; do
       set -- "${_args[@]}" "$@";
       unset _arg _args _i;
       continue;
-    ;;
+      ;;
     --*=*)
       _arg="$1";
       shift;
       set -- "${_arg%%=*}" "${_arg#*=}" "$@";
       unset _arg;
       continue;
-    ;;
+      ;;
     -u|--usage)    usage;    exit 0; ;;
     -h|--help)     usage -f; exit 0; ;;
     -v|--version)  echo "$_version"; exit 0; ;;
+    -j|--json)     _JSON=:; ;;
+    -m|--more)     _MORE=:; ;;
     --) shift; break; ;;
     -?|--*)
-      echo "$_as_me: Unrecognized option: '$1'" >&2;
+      echo "$_as_me: Unrecognized option: '$1'." >&2;
       printf '\n' >&2;
       usage -f >&2;
       exit 1;
     ;;
-
-    help)
-      if [[ "$#" -lt 2 ]]; then
-        echo "$_as_me: Missing argument for \`help'." >&2;
-        printf '\n' >&2;
-        usage >&2;
-        exit 1;
-      fi
-      case "$2" in
-        list) exec "$SDIR/list/list-pdefs.sh" --help; ;;
-        show) exec "$SDIR/show/show-pdefs.sh" --help; ;;
-        *)
-          echo "$_as_me help: Unrecognized subcommand: \`$2'." >&2;
-          printf '\n' >&2;
-          usage >&2;
-          exit 1;
-        ;;
-      esac
-      shift;
+    [!@]*@*|@*/*@*)
+      _IDENT="${1%@*}";
+      _VERSION="${1##*@}";
     ;;
-
-    list)
-      shift;
-      exec "$SDIR/list/list-pdefs.sh" "$@";
+    [!@]*/*|@*/*/*)
+      _IDENT="${1%/*}";
+      _VERSION="${1##*/}";
     ;;
-
-    show)
-      shift;
-      exec "$SDIR/show/show-pdefs.sh" "$@";
-    ;;
-
     *)
-      echo "$_as_me: Unexpected argument(s) '$*'." >&2;
-      printf '\n' >&2;
-      usage -f >&2;
-      exit 1;
+      if [[ -n "${_IDENT:-}${_VERSION:-}" ]]; then
+        echo "$_as_me: Unexpected argument(s) '$*'." >&2;
+        printf '\n' >&2;
+        usage -f >&2;
+        exit 1;
+      elif [[ "$#" -lt 2 ]]; then
+        echo "$_as_me: Missing argument to indicate VERSION." >&2;
+        printf '\n' >&2;
+        usage -f >&2;
+        exit 1;
+      else
+        _IDENT="$1";
+        shift;
+        _VERSION="$1";
+      fi
     ;;
   esac
   shift;
@@ -150,10 +142,38 @@ done
 
 # ---------------------------------------------------------------------------- #
 
-echo "$_as_me: No command given." >&2;
-printf '\n' >&2;
-usage >&2;
-exit 1;
+# Load common helpers
+
+. "${_FLOCO_COMMON_SH:-${BASH_SOURCE[0]%/*}/../common.sh}";
+
+
+# ---------------------------------------------------------------------------- #
+
+_NIX_APPLY=;
+if [[ -n "${_MORE:-}" ]]; then
+  _NIX_APPLY='pdef: removeAttrs pdef [
+    "sourceInfo" "_export" "metaFiles" "deserialized"
+  ]';
+else
+  _NIX_APPLY='pdef: pdef._export';
+fi
+
+
+# ---------------------------------------------------------------------------- #
+
+runShow() {
+  flocoEval                                             \
+    "${_NIX_ARGS[@]}" "$@"                              \
+    "mod.config.floco.pdefs.\"$_IDENT\".\"$_VERSION\""  \
+    --apply "$_NIX_APPLY"                               \
+  ;
+}
+
+if [[ -n "${_JSON:-}" ]]; then
+  runShow --json "$@"|$JQ;
+else
+  runShow "$@";
+fi
 
 
 # ---------------------------------------------------------------------------- #
