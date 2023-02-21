11f845f54ba4c3df665b1107cd63375edea49857
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-21 09:18:40 -0600

    use prev.lib in overlay for overridables

--- overlay.nix
+++ overlay.nix
@@ -41,16 +41,15 @@ final: prev: {
     pkgsFor = final;
   };
 
-  floco-nix = import ./pkgs/nix-plugin {
-    nixpkgs   = throw "floco: Nixpkgs should not be referenced from flake";
-    nix-flake = throw "floco: Nix should not be referenced from flake";
-    inherit (final) stdenv system boost treeFor semver bash darwin;
-    pkgsFor = final;
-    npm     = final.nodejs-14_x.pkgs.npm;
-    nix     = final.nixVersions.nix_2_12;
-  };
+  floco-nix =
+    prev.lib.makeOverridable ( import ./pkgs/nix-plugin/pkg-fun.nix ) {
+      inherit (final) stdenv boost treeFor semver bash darwin;
+      nodejs = final.nodejs-14_x;
+      npm    = final.nodejs-14_x.pkgs.npm;
+      nix    = final.nixVersions.nix_2_12;
+    };
 
-  floco = import ./pkgs/cli/pkg-fun.nix {
+  floco = prev.lib.makeOverridable ( import ./pkgs/cli/pkg-fun.nix ) {
     inherit (final) lib stdenv bash coreutils gnugrep jq makeWrapper;
     nix = final.nixVersions.nix_2_12;
   };
