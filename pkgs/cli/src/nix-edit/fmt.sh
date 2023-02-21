9f3150300c3d9e5ab6e7a68e1a9c9605490125cc
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-21 10:51:16 -0600

    CLI helpers for rewriting/formatting Nix files

---
+++ pkgs/cli/src/nix-edit/fmt.sh
@@ -0,0 +1,73 @@
+#! /usr/bin/env bash
+# ============================================================================ #
+#
+# Helper function to escape reserved keys in a Nix attribute set.
+#
+# ---------------------------------------------------------------------------- #
+
+: "${NIX:=nix}";
+: "${SED:=sed}";
+
+export NIX SED;
+
+
+# ---------------------------------------------------------------------------- #
+
+declare -a _nix_edit_escape_keywords;
+_nix_edit_escape_keywords=(
+  'assert'
+  'throw'
+  'with'
+  'let'
+  'in'
+  'or'
+  'inherit'
+  'rec'
+  'import'
+);
+
+
+# ---------------------------------------------------------------------------- #
+
+_nix_keyword_escape() {
+  local _patt_from _npatts;
+  _npatts="${#_nix_edit_escape_keywords[@]}";
+  _patt_from=" \(";
+  for k in "${_nix_edit_escape_keywords[@]:0:$(( _npatts - 1 ))}"; do
+    _patt_from="$_patt_from$k\|";
+  done;
+  _patt_from="$_patt_from${_nix_edit_escape_keywords[$(( _npatts - 1 ))]}\) =";
+  $SED "s/$_patt_from/ \"\\1\" =/g" "${@:-/dev/stdin}";
+}
+
+
+# ---------------------------------------------------------------------------- #
+
+_nix_fmt_file() {
+  $NIX eval --raw -f "${1:-/dev/stdin}" --apply 'e: let
+    nixpkgs = builtins.getFlake "nixpkgs";
+    inherit (nixpkgs) lib;
+  in ( lib.generators.toPretty {} e ) + "\n"
+  '|_nix_keyword_escape;
+}
+
+
+# ---------------------------------------------------------------------------- #
+
+_nix_fmt_rewrite() {
+  # For `mktmpAuto'.
+  . "${BASH_SOURCE%/*}/../common.sh";
+  local _tmpfile;
+  _tmpfile="$( mktmpAuto; )";
+  for f in "$@"; do
+    _nix_fmt_file "$f" > "$_tmpfile";
+    mv "$_tmpfile" "$f";
+  done
+}
+
+
+# ---------------------------------------------------------------------------- #
+#
+#
+#
+# ============================================================================ #
