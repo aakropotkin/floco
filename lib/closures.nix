3b2c30b60f5088d79cf160ddc1846533878e67db
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-25 20:35:14 -0600

    draft of treeInfoFromClosure

--- lib/closures.nix
+++ lib/closures.nix
@@ -34,10 +34,39 @@
 
 # ---------------------------------------------------------------------------- #
 
+  # XXX: This routine is an incomplete draft.
+  # Produce a "hoisted" `treeInfo' record from a closure.
+  __treeInfoFromClosure = closure: rootKey: let
+    noRoot      = builtins.filter ( pdef: pdef.key != rootKey ) closure;
+    idGroups    = builtins.groupBy ( pdef: pdef.ident ) noRoot;
+    topLevel    = builtins.mapAttrs ( _: builtins.head ) idGroups;
+    isSingleton = id: builtins.length ( idGroups.${id} ) == 1;
+    sparted     =
+      builtins.partition isSingleton ( builtins.attrNames idGroups );
+    markTopRequires = ident: pdef: let
+      multis   = removeAttrs pdef.depInfo sparted.right;
+      requires =
+        lib.filterAttrs ( di: de: topLevel.${di}.version != de.pin ) multis;
+    in pdef // { inherit requires; done = requires == {}; };
+    top  = builtins.mapAttrs markTopRequires topLevel;
+    rest = let
+      proc = ident: pdefs: let
+        mkEnt = pdef: {
+          name  = pdef.version;
+          value = markTopRequires ident pdef;
+        };
+      in builtins.listToAttrs ( map mkEnt ( builtins.tail pdefs ) );
+    in builtins.mapAttrs proc ( removeAttrs idGroups sparted.right );
+  in { inherit top rest; };
+
+
+# ---------------------------------------------------------------------------- #
+
 in {
 
   inherit
     pdefClosure
+    __treeInfoFromClosure
   ;
 
 }
