66811605b22ee5dff2141c5f9b7800d0aef7f8c5
Alex Ameen <alex.ameen.tx@gmail.com>
2023-02-26 11:48:06 -0600

    __treeInfoFrom{Pdefs,Closure} draft progress

--- lib/closures.nix
+++ lib/closures.nix
@@ -34,30 +34,106 @@
 
 # ---------------------------------------------------------------------------- #
 
-  # XXX: This routine is an incomplete draft.
+  # Tree Builder Node.
+  __mkTBNode = {
+    ident    ? dirOf key
+  , version  ? baseNameOf key
+  , key      ? ident + "/" + version
+  , depInfo  ? {}
+  , peerInfo ? {}
+
+  , path     ? "node_modules/" + ident
+  , isRoot   ? ! ( lib.hasInfix "node_modules/" path )
+  , pscope   ? if isRoot then {} else { ${ident} = version; }
+  , requires ?
+    lib.filterAttrs ( di: de: ( pscope.${di} or null ) == de.pin )
+                    depInfo
+  , children ? removeAttrs depInfo ( builtins.attrNames requires )
+  , ...
+  } @ args: let
+    children' = builtins.mapAttrs ( di: de: de.pin or de ) children;
+  in {
+    inherit ident version key depInfo peerInfo path isRoot pscope;
+    requires = builtins.mapAttrs ( di: de: de.pin or de ) requires;
+    children = children';
+    scope    = pscope // children';
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  __mkSubtree = closure: node: let
+    getPdef = { ident, version, ... }: builtins.head ( builtins.filter ( p:
+      ( p.ident == ident ) && ( p.version == version )
+    ) closure );
+    pprefix = if node.path == "" then "" else node.path + "/";
+    proc = ident: pin: let
+      cnode = __mkTBNode ( ( getPdef { inherit ident; version = pin; } ) // {
+        path   = pprefix + "node_modules/" + ident;
+        isRoot = false;
+        pscope = node.scope;
+      } );
+      value = __mkSubtree closure cnode;
+    in { name = value.path; inherit value; };
+    subtrees = builtins.mapAttrs proc node.children;
+  in node // {
+    subtree = builtins.listToAttrs ( builtins.attrValues subtrees );
+  };
+
+
+# ---------------------------------------------------------------------------- #
+
+  __topScope = closure: rootKey: let
+    noRoot   = builtins.filter ( pdef: pdef.key != rootKey ) closure;
+    idGroups = builtins.groupBy ( pdef: pdef.ident ) noRoot;
+  in builtins.mapAttrs ( _: pdefs: ( builtins.head pdefs ).version ) idGroups;
+
+
+# ---------------------------------------------------------------------------- #
+
+  __mkTree = closure: rootPdef: let
+    top      = __topScope closure rootPdef.key;
+    rootNode = ( __mkTBNode ( rootPdef // {
+      path     = "";
+      isRoot   = true;
+      requires = {};
+      children = top;
+    } ) ) // { scope = top; };
+    subtrees = builtins.genericClosure {
+      startSet = [( __mkSubtree closure rootNode )];
+      operator = node: lib.collect ( v: v ? subtree ) node.subtree;
+    };
+  in builtins.foldl' ( acc: node: acc // node.subtree ) {} subtrees;
+
+
+# ---------------------------------------------------------------------------- #
+
+  # XXX: Incomplete draft
   # Produce a "hoisted" `treeInfo' record from a closure.
-  __treeInfoFromClosure = closure: rootKey: let
-    noRoot      = builtins.filter ( pdef: pdef.key != rootKey ) closure;
-    idGroups    = builtins.groupBy ( pdef: pdef.ident ) noRoot;
-    topLevel    = builtins.mapAttrs ( _: builtins.head ) idGroups;
-    isSingleton = id: builtins.length ( idGroups.${id} ) == 1;
-    sparted     =
-      builtins.partition isSingleton ( builtins.attrNames idGroups );
-    markTopRequires = ident: pdef: let
-      multis   = removeAttrs pdef.depInfo sparted.right;
-      requires =
-        lib.filterAttrs ( di: de: topLevel.${di}.version != de.pin ) multis;
-    in pdef // { inherit requires; done = requires == {}; };
-    top  = builtins.mapAttrs markTopRequires topLevel;
-    rest = let
-      proc = ident: pdefs: let
-        mkEnt = pdef: {
-          name  = pdef.version;
-          value = markTopRequires ident pdef;
-        };
-      in builtins.listToAttrs ( map mkEnt ( builtins.tail pdefs ) );
-    in builtins.mapAttrs proc ( removeAttrs idGroups sparted.right );
-  in { inherit top rest; };
+  # TODO: Mark `dev' and `optional' fields.
+  # TODO: Audit `peerDepInfo'.
+  # TODO: Don't include transitive `devDependencies'.
+  # TODO: Hoist subtrees when possible to deduplicate. ( `yarn' strategy )
+  __treeInfoFromClosure = closure: rootPdef: let
+    tree = __mkTree closure rootPdef;
+  in builtins.mapAttrs ( _: node: {
+    inherit (node) key;
+  } ) tree;
+
+
+# ---------------------------------------------------------------------------- #
+
+  __treeInfoFromPdefs' = pdefs: keylike: let
+    closure  = pdefClosure' pdefs keylike;
+    rootPdef = lib.libfloco.getPdef { inherit pdefs; } keylike;
+  in __treeInfoFromClosure closure rootPdef;
+
+  __treeInfoFromPdefs = {
+    config ? { floco.pdefs = pa; }
+  , floco  ? config.floco
+  , pdefs  ? floco.pdefs
+  , ...
+  } @ pa: keylike: __treeInfoFromPdefs' pdefs keylike;
 
 
 # ---------------------------------------------------------------------------- #
@@ -66,7 +142,12 @@ in {
 
   inherit
     pdefClosure
+    __mkTBNode
+    __mkSubtree
+    __topScope
+    __mkTree
     __treeInfoFromClosure
+    __treeInfoFromPdefs
   ;
 
 }
