8d9f33e1674f1b7029c934a2f3545d6a0f9e1751
Alex Ameen <alex.ameen.tx@gmail.com>
2023-04-22 09:29:02 -0500

    db: dump JSON

--- db/pdefs.sql
+++ db/pdefs.sql
@@ -85,43 +85,74 @@ CREATE TABLE IF NOT EXISTS sysInfoEngineEnts(
 
 -- -------------------------------------------------------------------------- --
 
-CREATE VIEW IF NOT EXISTS v_Pdefs (
+CREATE VIEW IF NOT EXISTS v_PdefsJSONV (
   key, ident, version, ltype, fetcher, fetchInfo
-, lifecycle_build, lifecycle_install
-, binInfo_binDir, binInfo_binPairs
-, depInfo, peerInfo, treeInfo
-, fsInfo_dir, fsInfo_gypfile, fsInfo_shrinkwrap
-, sysInfo_cpu, sysInfo_os, sysInfo_engines
+, lifecycle, binInfo, depInfo, peerInfo, fsInfo, sysInfo
 ) AS SELECT
-  p.key, p.ident, p.version, p.ltype, p.fetcher, p.fetchInfo
-, p.lifecycle_build, p.lifecycle_install, p.binInfo_binDir, p.binInfo_binPairs
+  p.key, p.ident, p.version, p.ltype, p.fetcher, json( p.fetchInfo )
+  -- lifecycle
+, json_object(
+    'build',   iif( p.lifecycle_build, json( 'true' ), json( 'false' ) )
+  , 'install', iif( p.lifecycle_install, json( 'true' ), json( 'false' ) ) )
+  -- binInfo
+, json_object( 'binDir',   p.binInfo_binDir
+             , 'binPairs', json( p.binInfo_binPairs ) )
   -- depInfo
-, iif( ( COUNT( di.ident ) <= 0 ), '{}'
-     , json_group_object( di.ident, json_object(
-                                      'descriptor', di.descriptor
-                                    , 'runtime',    di.runtime
-                                    , 'dev',        di.dev
-                                    , 'optional',   di.optional
-                                    , 'bundled',    di.bundled
-                                    ) ) )
+, iif( ( COUNT( di.ident ) <= 0 ), json( '{}' )
+     , json_group_object(
+         di.ident
+       , json_object(
+           'descriptor', di.descriptor
+         , 'runtime',    iif( di.runtime, json( 'true' ), json( 'false' ) )
+         , 'dev',        iif( di.dev, json( 'true' ), json( 'false' ) )
+         , 'optional',   iif( di.optional, json( 'true' ), json( 'false' ) )
+         , 'bundled',    iif( di.bundled, json( 'true' ), json( 'false' ) )
+         ) ) )
   -- peerInfo
-, iif( ( COUNT( pi.ident ) <= 0 ), '{}'
-     , json_group_object( pi.ident, json_object(
-                                      'descriptor', pi.descriptor
-                                    , 'optional',   pi.optional
-                                    ) ) )
-  -- TODO: treeInfo
-, '{}'
-, p.fsInfo_dir, p.fsInfo_gypfile, p.fsInfo_shrinkwrap
-, p.sysInfo_cpu, p.sysInfo_os
-  -- sysInfo_engines
-, json_group_object( sie.id, sie.value )
+, iif( ( COUNT( pi.ident ) <= 0 ), json( '{}' )
+     , json_group_object(
+         pi.ident
+       , json_object(
+           'descriptor', pi.descriptor
+         , 'optional',   iif( pi.optional, json( 'true' ), json( 'false' ) )
+         ) ) )
+  -- fsInfo
+, json_object(
+    'dir',        p.fsInfo_dir
+  , 'gypfile',    iif( p.fsInfo_gypfile, json( 'true' ), json( 'false' ) )
+  , 'shrinkwrap', iif( p.fsInfo_shrinkwrap, json( 'true' ), json( 'false' ) ) )
+  -- sysInfo
+, json_object(
+    'cpu',     json( p.sysInfo_cpu )
+  , 'os',      json( p.sysInfo_os )
+  , 'engines', iif( ( COUNT( sie.id ) <= 0 ), json( '{}' )
+                  , json_group_object( sie.id, sie.value )
+                  ) )
 FROM pdefs p
 LEFT JOIN depInfoEnts di        ON ( p.key = di.parent )
 LEFT JOIN peerInfoEnts pi       ON ( p.key = pi.parent  )
 LEFT JOIN sysInfoEngineEnts sie ON ( p.key = sie.parent )
 GROUP BY p.key;
 
+
+-- -------------------------------------------------------------------------- --
+
+-- SQL -> JSON
+-- sqlite3 <DB> 'SELECT JSON from v_PdefsJSONF'|jq [-s];
+CREATE VIEW IF NOT EXISTS v_PdefsJSONF ( key, JSON ) AS SELECT key, json_object(
+  'key',       p.key
+, 'ident',     p.ident
+, 'version',   p.version
+, 'ltype',     p.ltype
+, 'fetcher',   p.fetcher
+, 'fetchInfo', json( p.fetchInfo )
+, 'lifecycle', json( p.lifecycle )
+, 'binInfo',   json( p.binInfo )
+, 'depInfo',   json( p.depInfo )
+, 'peerInfo',  json( p.peerInfo )
+, 'fsInfo',    json( p.fsInfo )
+, 'sysInfo',   json( p.sysInfo )
+) FROM v_PdefsJSONV p;
 
 
 -- -------------------------------------------------------------------------- --
