62d6896ff3a3e08280d96445ba9d295722137f89
Alex Ameen <alex.ameen.tx@gmail.com>
2023-05-20 12:22:55 -0500

    load other sql schemas into c++

--- db/cli/pjs-core.cc
+++ db/cli/pjs-core.cc
@@ -1,24 +1,46 @@
+/* ========================================================================== *
+ *
+ *
+ *
+ * -------------------------------------------------------------------------- */
 
-#include "floco-sql.hh"
 #include <iostream>
 #include <sqlite3.h>
+
+#include "floco-sql.hh"
+
+
+/* -------------------------------------------------------------------------- */
 
   int
 main( int argc, char * argv[], char ** envp )
 {
-  sqlite3 * DB;
-  std::string sql = pjsCoreSchemaSQL;
-  int exit = 0;
-  exit = sqlite3_open( "example.db", & DB );
-  char * messaggeError;
-  exit = sqlite3_exec( DB, sql.c_str(), NULL, 0, &messaggeError );
+  sqlite3     * db;
+  char        * messageError;
+  std::string   sql = pjsCoreSchemaSQL;
+  int           err = 0;
 
-  if ( exit != SQLITE_OK ) {
-    std::cerr << "Error Create Table" << std::endl;
-    sqlite3_free( messaggeError );
-  }
+  err = sqlite3_open( "pjs-core.db", & db );
+  err = sqlite3_exec( db, sql.c_str(), NULL, 0, & messageError );
+
+  if ( err != SQLITE_OK )
+    {
+      std::cerr << "Error Create Table" << std::endl;
+      sqlite3_free( messageError );
+    }
   else
-    std::cout << "Table created Successfully" << std::endl;
-  sqlite3_close( DB );
-  return 0;
+    {
+      std::cout << "Table created Successfully" << std::endl;
+    }
+
+  sqlite3_close( db );
+
+  return err;
 }
+
+
+/* -------------------------------------------------------------------------- *
+ *
+ *
+ *
+ * ========================================================================== */
