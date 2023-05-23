637195340a7bdff24cce4b043d693856c92594f7
Alex Ameen <alex.ameen.tx@gmail.com>
2023-05-23 07:30:06 -0500

    PjsCore from URL

--- db/cli/pjs-core.hh
+++ db/cli/pjs-core.hh
@@ -11,6 +11,9 @@
 #include <filesystem>
 #include <utility>
 #include <stdexcept>
+#include <chrono>
+#include <cstdio>
+#include <fstream>
 
 #include <nlohmann/json.hpp>
 #include <unordered_map>
@@ -25,6 +28,10 @@ pathIsJSFile( std::string_view p )
   return ( p[l - 3] == '.' ) && ( p[l - 2] == 'j' ) && ( p[l - 1] == 's' );
 }
 
+
+/* -------------------------------------------------------------------------- */
+
+extern int curlFile( const char * url, const char * outFile );
 
 
 /* -------------------------------------------------------------------------- */
@@ -157,21 +164,86 @@ class BinInfo {
 
 /* -------------------------------------------------------------------------- */
 
-struct PjsCore {
+class PjsCore {
 
-  std::string    url;
-  unsigned long  timestamp;
-  std::string    name;
-  std::string    version;
-  nlohmann::json bin;
-  nlohmann::json dependencies;
-  nlohmann::json devDependencies;
-  nlohmann::json devDependenciesMeta;
-  nlohmann::json peerDependencies;
-  nlohmann::json peerDependenciesMeta;
-  nlohmann::json os;
-  nlohmann::json cpu;
-  nlohmann::json engines;
+  public:
+
+    std::string    url;
+    unsigned long  timestamp;
+    std::string    name;
+    std::string    version              = "0.0.0-0";
+    nlohmann::json bin                  = nlohmann::json::object();
+    nlohmann::json dependencies         = nlohmann::json::object();
+    nlohmann::json devDependencies      = nlohmann::json::object();
+    nlohmann::json devDependenciesMeta  = nlohmann::json::object();
+    nlohmann::json peerDependencies     = nlohmann::json::object();
+    nlohmann::json peerDependenciesMeta = nlohmann::json::object();
+    nlohmann::json os                   = nlohmann::json::array( { "*" } );
+    nlohmann::json cpu                  = nlohmann::json::array( { "*" } );
+    nlohmann::json engines              = nlohmann::json::object();
+
+
+  private:
+
+      void
+    init(       std::string_view   url
+        , const nlohmann::json   & json
+        ,       unsigned long      timestamp = std::time( nullptr )
+        )
+    {
+      this->url       = url;
+      this->timestamp = timestamp;
+      for ( auto & [key, value] : json.items() )
+        {
+          if ( key == "name" )              { this->name         = value; }
+          else if ( key == "version" )      { this->version      = value; }
+          else if ( key == "bin" )          { this->bin          = value; }
+          else if ( key == "dependencies" ) { this->dependencies = value; }
+          else if ( key == "os" )           { this->os           = value; }
+          else if ( key == "cpu" )          { this->cpu          = value; }
+          else if ( key == "engines" )      { this->engines      = value; }
+          else if ( key == "devDependencies" )
+            {
+              this->devDependencies = value;
+            }
+          else if ( key == "devDependenciesMeta" )
+            {
+              this->devDependenciesMeta = value;
+            }
+          else if ( key == "peerDependencies" )
+            {
+              this->peerDependencies = value;
+            }
+          else if ( key == "peerDependenciesMeta" )
+            {
+              this->peerDependenciesMeta = value;
+            }
+        }
+    }
+
+
+  public:
+
+    PjsCore(       std::string_view   url
+           , const nlohmann::json   & json
+           ,       unsigned long      timestamp = std::time( nullptr )
+           )
+    {
+      this->init( url, json, timestamp );
+    }
+
+    PjsCore( std::string_view url )
+    {
+      std::string   tmp = std::tmpnam( nullptr );
+      std::string   _url( url );
+      unsigned long timestamp = std::time( nullptr );
+      curlFile( _url.c_str(), tmp.c_str() );
+      std::ifstream f( tmp );
+      nlohmann::json json = nlohmann::json::parse( f );
+      f.close();
+      remove( tmp.c_str() );
+      this->init( url, json, timestamp );
+    }
 
 };
 
