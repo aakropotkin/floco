7ff421801273000056ef5d66f0051f9f3e62d17b
Alex Ameen <alex.ameen.tx@gmail.com>
2023-06-24 09:17:19 -0500

    add test case to suite

--- tests/modules/packages/nodePackage/check.sh
+++ tests/modules/packages/nodePackage/check.sh
@@ -21,7 +21,7 @@ TVERSION="$( $NIX shell -f "$SDIR/default.nix" -c test; )";
 case "$TVERSION" in
   v16.*) :; ;;
   *)
-    echo "FAIL: Expected version 16.x but executable has '$TVERSION'" >&2;
+    echo "fail: Expected version 16.x but executable has '$TVERSION'" >&2;
     exit 1;
   ;;
 esac
@@ -42,12 +42,12 @@ mapfile -t DRV_NODE_VERSIONS < <(
 
 case "${#DRV_NODE_VERSIONS[@]}" in
   0)
-    echo "FAIL: Expected a single node reference in closure, but none" >&2;
+    echo "fail: Expected a single node reference in closure, but none" >&2;
     exit 2;
   ;;
   1) :; ;;
   *)
-    echo "FAIL: Expected a single node reference in closure, but got:" >&2;
+    echo "fail: Expected a single node reference in closure, but got:" >&2;
     printf "  '%s'\n" "${DRV_NODE_VERSIONS[@]}" >&2;
     exit 3;
   ;;
@@ -59,7 +59,7 @@ esac
 case "${DRV_NODE_VERSIONS[0]}" in
   16.*) :; ;;
   *)
-    printf 'FAIL: Expected version 16.x but derivation references ' >&2;
+    printf 'fail: Expected version 16.x but derivation references ' >&2;
     echo "'${DRV_NODE_VERSIONS[0]}'" >&2;
     exit 4;
   ;;
